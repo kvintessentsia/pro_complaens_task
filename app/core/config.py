923fa1cfd9681d5f6f0c3124110c96fa8ac151ae
Владислав Черепков <trinitikvintessentsia@yandex.ru>
2023-09-26 18:45:36 +0300

    3

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     app_title: str = 'работа с csv файлом'
-    database_url: str
+    database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
 
     class Config:
         env_file = '.env'
